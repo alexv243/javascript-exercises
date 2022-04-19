10834cb9732bc0f79976f6b09471d07e3d93064b
Alex Volders <avoldermort@gmail.com>
2022-04-18 21:07:53 -0400

    function to sum between two numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,50 @@
-const sumAll = function() {
+const sumAll = function(intOne, intTwo) {
+    let array = [];
+
+    if (!Number.isInteger(intOne) || !Number.isInteger(intTwo))
+    {
+        return "ERROR";
+    }
+
+    else if ((intOne || intTwo) < 0)
+    {
+        return "ERROR";
+    }
+    
+    else if (intOne > intTwo)
+    {
+        for (let i = intOne; i >= intTwo; i--)
+        {
+            array.push(i);
+        }  
+
+        let sum = 0;
+
+        for (let i = 0; i < array.length; i++)
+        {
+            sum += array[i];
+        }
+
+        return sum;
+    }
+
+    else if (intOne < intTwo)
+    {
+        for(let i = intTwo; i >= intOne; i--)
+        {
+            array.push(i);
+        }
+
+        let sum = 0;
+
+        for (let i = 0; i < array.length; i++)
+        {
+            sum += array[i];
+        }
+
+        return sum;
+    }
+    
 
 };
 
